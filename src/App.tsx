85bd37813bb37aeeb44ddf0b18dfe4225a5c088a
Philipicalt <philipi.altoe@gmail.com>
2022-06-10 22:07:58 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { useArray, UseArrayActions } from './hooks/useArray';
+import ListaInstrucoes from './ListaInstrucoes';
+import TabelasInstrucoes from './TabelasInstrucoes';
+import Tabelas from './TabelasInstrucoes';
+
+
+interface IInstrucoes {
+	nome: string;
+	enviada: boolean;
+	executada: boolean;
+	escrita: boolean;
+	entrada1: string;
+	entrada2: string;
+	entrada3?: string;
+}
+
+interface IEstacaoReserva {
+	nome: string;
+	ocupada: boolean;
+	operacao: string;//Ver se é melhor criar uma tipagem para isso
+	Vj: number;
+	Vk: number;
+	Qj: number;
+	Qk: number;
+	A: string;
+}
+
+interface IRegistrador {
+	nome: string;
+	valor: string;
+}
+
+export interface IIntrucaoContextProps {
+	arrInstrucoes: UseArrayActions<IInstrucoes>;
+	arrEstacaoReserva: UseArrayActions<IEstacaoReserva>;
+	arrRegistrador: UseArrayActions<IRegistrador>;
+}
+
+export const IntrucaoContext = React.createContext<IIntrucaoContextProps>({} as IIntrucaoContextProps);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const arrInstrucoes = useArray<IInstrucoes>([]);
+	const arrEstacaoReserva = useArray<IEstacaoReserva>([]);
+	const arrRegistrador = useArray<IRegistrador>([]);
+
+	const defaultValue: IIntrucaoContextProps = {
+		arrInstrucoes,
+		arrEstacaoReserva,
+		arrRegistrador
+	}
+
+	const onMount = () => {
+		if (arrInstrucoes.length) return;
+		const arrInstrucoesAux: IInstrucoes[] = [
+			{
+				nome: 'Add',
+				enviada: true,
+				escrita: true,
+				executada: true,
+				entrada1: 'F0',
+				entrada2: '10',
+				entrada3: 'F1'
+			},
+			{
+				nome: 'Mult',
+				enviada: true,
+				escrita: true,
+				executada: false,
+				entrada1: 'F2',
+				entrada2: 'F0',
+				entrada3: '10'
+			},
+			{
+				nome: 'Sub',
+				enviada: true,
+				escrita: false,
+				executada: false,
+				entrada1: 'F1',
+				entrada2: 'F2',
+				entrada3: 'F0'
+			},
+			{
+				nome: 'Ld',
+				enviada: false,
+				escrita: false,
+				executada: false,
+				entrada1: 'F0',
+				entrada2: '20',
+			},
+		]
+
+		arrInstrucoes.setValue(arrInstrucoesAux);
+	}
+	useEffect(onMount, []);
+
+	return (
+		<WrapperSiderContent>
+			<IntrucaoContext.Provider value={defaultValue}>
+				<div className='container-direita'>
+					<ListaInstrucoes />
+				</div>
+				<div className='container-esquerda'>
+					<TabelasInstrucoes />
+				</div>
+			</IntrucaoContext.Provider>
+		</WrapperSiderContent>
+	);
 }
 
 export default App;
+
+
+const WrapperSiderContent = styled.div`
+	display: flex;
+	flex-direction: row;
+	/* height: 100%;
+	flex: 1; */
+
+	.container-direita{
+		width: 30%;
+		height: 100%;
+		background: #00fff278;
+	}
+	.container-esquerda{
+		width: 70%;
+		height: 100%;
+		background: #01a54088;
+	}
+`;
---
+++ src/TabelasInstrucoes.tsx
@@ -0,0 +1,166 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { IntrucaoContext } from './App';
+
+
+const TabelasInstrucoes: React.FC = () => {
+    const {
+        arrEstacaoReserva,
+        arrInstrucoes,
+        arrRegistrador
+    } = useContext(IntrucaoContext);
+
+    return (
+        <Wrapper>
+            <WrapperTabelas>
+                <label>
+                    Instruções
+                </label>
+                <STabela>
+                    <thead>
+                        <tr>
+                            <th>Instrução</th>
+                            <th>Enviada</th>
+                            <th>Executada</th>
+                            <th>Escrita</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            (arrInstrucoes && arrInstrucoes.value && arrInstrucoes.value.length)
+                                ? arrInstrucoes.value.map(instrucao =>
+                                    <tr>
+                                        <td>{instrucao.nome}</td>
+                                        <td>{instrucao.enviada ? 'X' : ''}</td>
+                                        <td>{instrucao.escrita ? 'X' : ''}</td>
+                                        <td>{instrucao.executada ? 'X' : ''}</td>
+                                    </tr>
+                                )
+                                :
+                                <tr>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                </tr>
+                        }
+                    </tbody>
+                </STabela>
+            </WrapperTabelas>
+            <WrapperTabelas>
+                <label>
+                    Estação de Reserva
+                </label>
+                <STabela>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Ocupada</th>
+                            <th>Operação</th>
+                            <th>Vj</th>
+                            <th>Vk</th>
+                            <th>Qj</th>
+                            <th>Qk</th>
+                            <th>A</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            (arrEstacaoReserva && arrEstacaoReserva.value && arrEstacaoReserva.value.length)
+                                ? arrEstacaoReserva.value.map(estacaoReserva =>
+                                    <tr>
+                                        <td>{estacaoReserva.nome}</td>
+                                        <td>{estacaoReserva.ocupada ? 'X' : ''}</td>
+                                        <td>{estacaoReserva.operacao}</td>
+                                        <td>{estacaoReserva.Vj}</td>
+                                        <td>{estacaoReserva.Vk}</td>
+                                        <td>{estacaoReserva.Qj}</td>
+                                        <td>{estacaoReserva.Qk}</td>
+                                        <td>{estacaoReserva.A}</td>
+                                    </tr>
+                                )
+                                :
+                                <tr>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>--</td>
+                                </tr>
+                        }
+                    </tbody>
+                </STabela>
+            </WrapperTabelas>
+            <WrapperTabelas>
+                <label>
+                    Registradores
+                </label>
+                <STabela>
+                    {
+                        (arrRegistrador && arrRegistrador.value && arrRegistrador.value.length)
+                            ? arrRegistrador.value.map(registrador =>
+                                <>
+                                    <thead>
+                                        <tr>
+                                            <th>{registrador.nome}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{registrador.valor}</td>
+                                        </tr>
+                                    </tbody>
+                                </>
+                            )
+                            :
+                            <>
+                                <thead>
+                                    <tr>
+                                        <th>--</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>--</td>
+                                    </tr>
+                                </tbody>
+                            </>
+                    }
+                </STabela>
+            </WrapperTabelas>
+        </Wrapper>
+    );
+}
+
+export default TabelasInstrucoes;
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+const WrapperTabelas = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin-top: 100px;
+`;
+
+const STabela = styled.table`
+	border: 1px solid black;
+	th{
+		border: 1px solid gray;
+		padding: 10px;
+	}
+	td { 
+		border: 1px solid gray;
+		padding: 10px;
+	}
+`;
+
